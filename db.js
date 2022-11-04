7bdeb5235f070a1eae89c022dfa858aaddee45b6
Zyad <Zyad>
2022-11-04 23:47:40 +0200

    latest update

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    host: "localhost",
+    host: "http://144.24.209.19:9090/",
     database: "salam",
     password: "ZozSaber@0909010169",
     port: 5432,

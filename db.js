b99d647d5e254c0eff866ae58626fa9c46074efa
ZyadSaber <zyad.2002.best@gmail.com>
2023-02-16 18:19:08 +0200

    add new build

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    host: "144.24.209.19",
+    // host: "144.24.209.19",
+    host: "127.0.0.1",
+    // host: "192.168.1.250",
     database: "salam",
     password: "ZozSaber@0909010169",
     port: 5432,

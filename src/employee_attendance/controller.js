b6c6673193ca1ba96d3f070e14d4a9e20921532d
ZyadSaber <zyad.2002.best@gmail.com>
2023-02-17 05:39:42 +0200

    add updaets

--- src/employee_attendance/controller.js
+++ src/employee_attendance/controller.js
@@ -7,11 +7,17 @@ const postEmployeeAttendance = (req, res) =>{
     const {query_status, employee_id, date, attendance_time, reason, absent} = req.body;
     if (query_status === "n") {
         if(!date){
-            res.send({response: "cannot insert null into date"})
+            res.status(500).send({response: "cannot insert null into date"})
+            return;
         } else if (!employee_id){
-            res.send({response: "cannot insert null into employee_id"})
+            res.status(500).send({response: "cannot insert null into employee_id"})
+            return;
         } else if (!absent){
-            res.send({response: "cannot insert null into absent"})
+            res.status(500).send({response: "cannot insert null into absent"})
+            return;
+        } else if (!attendance_time){
+            res.status(500).send({response: "cannot insert null into attendance_time"})
+            return;
         } else if (attendance_time) {
             pool.query(employees.getEmployeeAttendanceTime, [employee_id], (error, result) => {
                 if(error){
@@ -19,15 +25,25 @@ const postEmployeeAttendance = (req, res) =>{
                     return;
                 }
                 const employeeAttendanceTime = result.rows[0].attendance_time
-                const realHour = employeeAttendanceTime.slice(0,2)
-                const realMinute = employeeAttendanceTime.slice(3,5)
-                const attendanceHour = attendance_time.slice(0,2)
-                const attendanceMinute = attendance_time.slice(3,5)
-                console.log(realMinute)
-                console.log(attendanceMinute)
-                console.log(+(realMinute-attendanceMinute))
-                const finalTime = `${+(realHour-attendanceHour)}:${+(realMinute-attendanceMinute)}`
-                console.log(finalTime)
+                const attend_time = new Date(`${date} ${attendance_time}`)
+                const expedited_time = new Date(`${date} ${employeeAttendanceTime}`)
+                const diffHours = attend_time.getHours() - expedited_time.getHours() ;
+                const diffMinutes =  attend_time.getMinutes() - expedited_time.getMinutes();
+                let finalTIme 
+                if(diffHours < 0){
+                    finalTIme = 0
+                } else if(diffMinutes < 0){
+                    finalTIme = (diffHours * 60) + (diffMinutes * -1)
+                } else {
+                    finalTIme = (diffHours * 60) + (diffMinutes)
+                }
+                pool.query(queries.addEmployeeAttendance, [employee_id, date, attendance_time, reason, absent, finalTIme], (error, result)=>{
+                    if(error){
+                        res.status(500).send({response: error});
+                        return;
+                    }
+                    res.send({response: "success"})
+                })
             })
         }
     }
--- src/employee_attendance/queries.js
+++ src/employee_attendance/queries.js
@@ -1,4 +1,4 @@
-const addEmployeeAttendance = "INSERT INTO employee_attendance (employee_id, date, attendance_time, reason, absent) VALUES ($1,$2, $3, $4, $5)"
+const addEmployeeAttendance = "INSERT INTO employee_attendance (employee_id, date, attendance_time, reason, absent, late_time) VALUES ($1,$2, $3, $4, $5, $6)"
 
 module.exports =  {
     addEmployeeAttendance

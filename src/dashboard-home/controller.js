cb1d8482c4244ac8af794031fd739480c73fc7ef
ZyadSaber <zyad.2002.best@gmail.com>
2023-05-05 23:58:34 +0000

    new update

--- src/dashboard-home/controller.js
+++ src/dashboard-home/controller.js
@@ -3,20 +3,20 @@ const queries = require('./queries');
 
 const getDashBoard = (req, res)=>{
     res.send({
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [
             {
-                label: '# of votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    "rgba(75,192,192,1)",
-                    "#ecf0f1",
-                    "#50AF95",
-                    "#f3ba2f",
-                    "#2a71d0",
-                ],
-                borderColor: "black",
-                borderWidth: 2,
+                label: '',
+                data: [],
+                // backgroundColor: [
+                //     "rgba(75,192,192,1)",
+                //     "#ecf0f1",
+                //     "#50AF95",
+                //     "#f3ba2f",
+                //     "#2a71d0",
+                // ],
+                // borderColor: "black",
+                // borderWidth: 2,
             },
         ],
     })

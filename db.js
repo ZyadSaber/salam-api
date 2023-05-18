5b119cb60d18a93db393da61e0683e537bebd16e
ZyadSaber <zyad.2002.best@gmail.com>
2023-05-18 15:32:32 +0000

    new salam build

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    host: "144.24.209.19",
+    // host: "144.24.209.19",
     // host: "127.0.0.1",
-    // host: "192.168.1.250",
+    host: "192.168.1.250",
     database: "salam",
-    password: "ZozSaber@0909010169",
+    password: "123456",
     port: 5432,
 });
 

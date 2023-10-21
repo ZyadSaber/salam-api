b4a3dce75a2a093c4ec16d3f3198aee6d6ac682e
ZyadSaber <zyad.2002.best@gmail.com>
2023-10-21 12:58:29 +0000

    update

--- src/system-pdf-generate/system-pdf-generate.controller.ts
+++ src/system-pdf-generate/system-pdf-generate.controller.ts
@@ -3,8 +3,6 @@ import {
     Get,
     Res
   } from '@nestjs/common';
-  import { createReadStream } from 'fs';
-  import { join } from 'path';
 import {SystemPdfGenerateService} from "./system-pdf-generate.service"
 
 @Controller('system_pdf_generate')
@@ -12,7 +10,6 @@ export class SystemPdfGenerateController {
     constructor(private systemPdfGenerateService: SystemPdfGenerateService) {}
     @Get('files')
     getFile(@Res() res: any) {
-      const file = createReadStream(join(process.cwd(), 'src/system-pdf-generate/pdf/sample.pdf'));
-      file.pipe(res);
+      return this.systemPdfGenerateService.invoice_search_pdf(res)
     }
 }
--- src/system-pdf-generate/system-pdf-generate.service.ts
+++ src/system-pdf-generate/system-pdf-generate.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @Injectable()
-export class SystemPdfGenerateService {}
+export class SystemPdfGenerateService {
+    
+    async invoice_search_pdf(res: any){
+        const file = createReadStream(join(process.cwd(), 'src/system-pdf-generate/pdf/sample.pdf'));
+      file.pipe(res);
+    }
+}

df9f9acfe2d9c0e2cf29fe40ef137755117879d4
ZyadSaber <zyad.2002.best@gmail.com>
2023-11-07 10:17:49 +0000

    upate

--- src/charts/charts.service.ts
+++ src/charts/charts.service.ts
@@ -17,7 +17,6 @@ export class ChartsService {
         },
       },
     });
-    // console.log(customersData);
     const supplierData = await this.prisma.supplier_invoices.findMany({
       where: {
         supplier_invoice_date: {
@@ -25,7 +24,6 @@ export class ChartsService {
         },
       },
     });
-    // .$queryRaw`SELECT * FROM customers_data`;
     const chart = {
       labels: ['Suppliers', 'Customers'],
       datasets: [
@@ -33,20 +31,12 @@ export class ChartsService {
           label: 'Invoices',
           data: [supplierData.length, customersData.length],
           backgroundColor: [
-            // 'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
-            // 'rgba(255, 206, 86, 0.2)',
-            // 'rgba(75, 192, 192, 0.2)',
             'rgba(153, 102, 255, 0.2)',
-            // 'rgba(255, 159, 64, 0.2)',
           ],
           borderColor: [
-            // 'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
-            // 'rgba(255, 206, 86, 1)',
-            // 'rgba(75, 192, 192, 1)',
             'rgba(153, 102, 255, 1)',
-            // 'rgba(255, 159, 64, 1)',
           ],
           borderWidth: 1,
         },
@@ -83,6 +73,10 @@ export class ChartsService {
         }
       }
     })
+    const sumCustomers = paidCustomers.length !== 0 ? paidCustomers.reduce(
+      //@ts-ignore
+      ({customer_invoice_paid: pervPain}, {customer_invoice_paid: currentPain}) => +pervPain + +currentPain
+    ) : 0;
     const paidSuppliers= await this.prisma.supplier_invoices.findMany({
       where:{
         supplier_invoice_date: {
@@ -93,12 +87,17 @@ export class ChartsService {
         }
       }
     })
+    const sumSuppliers = paidSuppliers.length !== 0 ? paidSuppliers.reduce(
+      //@ts-ignore
+      ({supplier_invoice_paid: pervPain}, {supplier_invoice_paid: currentPain}) => {+pervPain + +currentPain}
+    ) : 0
+
     const chart = {
       labels: ['Incomes', 'Outcomes', 'suppliers', 'customers'],
       datasets: [
         {
           label: 'Invoices',
-          data: [cashPaymentVoucher.length, cashReceiptVoucher.length, paidSuppliers.length, paidCustomers.length],
+          data: [cashPaymentVoucher.length, cashReceiptVoucher.length, sumSuppliers, sumCustomers],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
